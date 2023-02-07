99b4a3167f9603b6c5b9ef9b904276bd83d33991
leewebstechnologies <aliuajibade317@gmail.com>
2023-02-06 18:03:56 -0800

    header folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <div className="App">
       <h1>Let's build the TINDER Clone App 🚀!</h1>
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      {/* Buttons below Tinder cards */}
+
+      {/* Chats screen */}
+      {/* Individual chat screen */}
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+import "./header.css";
+import PersonIcon from '@mui/icons-material/Person';
+const Header = () => {
+  return (
+    <div>
+      <h2>I am a header</h2>
+    </div>
+  );
+};
+
+export default Header;
